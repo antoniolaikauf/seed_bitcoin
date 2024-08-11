a6b6898f057ea2c182a4cf43c72eb9b61a74410d
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-08-11 20:32:42 +0200

    seed phrase

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 transazione bitcoin 
+
+generazione seed 
+![](img/mbc3_0504.png)
+
+generazione private key
+![](img/mbc3_0505.png)
--- seed.py
+++ seed.py
@@ -6,25 +6,21 @@ def bits_entropy():
 
 bits_hex=bits_entropy()
 
-sha256=hashlib.sha256() # put inside sha256
+sha256=hashlib.sha256() # sha256 for checksum
 sha256.update(bits_hex)
 sha256=sha256.hexdigest()
+checksum=bin(int(sha256[:1],16))[2:].zfill(4) # first 4 bits of sha256
 
 bits=''.join(format(bytes,'08b') for bytes in bits_hex) # bytes hex to bits 
 
-
-checksum=bin(int(sha256[:1],16))[2:].zfill(4) # first 4 bits of sha256
 hex_string=bits + checksum
-# print(len(hex_string))
-
 array_bits_words=[hex_string[i : i + 11] for i in range(0,len(hex_string), 11)]
-print(array_bits_words)
 
 with open('words.txt', mode='r') as f:
     words=f.readlines()
     seed_phrase=''
 
-    for x in range(12):# seed prhase 12
+    for x in range(12): # seed prhase 12
         extracted_bits= array_bits_words[x]
 
         index_word=int(extracted_bits,2)
