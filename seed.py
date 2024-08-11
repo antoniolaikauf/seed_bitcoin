127d88495992fb96e937d17c78298ecdf1fc3b95
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-08-11 20:27:40 +0200

    seed phrase

--- seed.py
+++ seed.py
@@ -15,9 +15,20 @@ bits=''.join(format(bytes,'08b') for bytes in bits_hex) # bytes hex to bits
 
 checksum=bin(int(sha256[:1],16))[2:].zfill(4) # first 4 bits of sha256
 hex_string=bits + checksum
-print(len(hex_string))
+# print(len(hex_string))
 
+array_bits_words=[hex_string[i : i + 11] for i in range(0,len(hex_string), 11)]
+print(array_bits_words)
 
+with open('words.txt', mode='r') as f:
+    words=f.readlines()
+    seed_phrase=''
 
-# array_bits_words=[hex_bits[i : i + 11] for i in range(0,len(hex_bits) - 11, 11)]
-# print(array_bits_words)
+    for x in range(12):# seed prhase 12
+        extracted_bits= array_bits_words[x]
+
+        index_word=int(extracted_bits,2)
+
+        seed_phrase+= ' ' +  words[index_word]
+    
+    print(seed_phrase)
